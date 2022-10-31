8653a615e73f08b9556aa64998b8e36b455beca1
Cellinnek <Cysielek@outlook.com>
2022-10-31 19:50:25 +0100

    Ver 1.0000001

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::ops::Index;
-use minifb::{Key, MouseButton, MouseMode, Scale, Window, WindowOptions};
+use minifb::{Scale, Window, WindowOptions};
 
 const WIDTH: usize = 128;
 const HEIGHT: usize = 128;
+
+fn line(buffer: &mut Vec<u32>, x1: i32, y1: i32, x2: i32, y2: i32, color: u32) {
+    for i in 0..buffer.len(){ buffer[i] = color; }
+}
 
 fn main() {
     let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
@@ -21,16 +24,13 @@ fn main() {
             println!("Unable to create window {}", err);
             return;
         }
-    };
-
-    let (mut width, mut height) = (WIDTH, HEIGHT);
+    }; //error handling
 
     while window.is_open() {
-        /*buffer[((200 /*y*/ as usize) * (width)) + 200 /*x*/ as usize] = 0x00ffffff;*/
-        for i in 0..buffer.len(){
-            buffer[i] = 0xffffffff;
-        }
+        /*buffer[((200 /*y*/ as usize) * (WIDTH)) + 200 /*x*/ as usize] = 0x00ffffff;*/
+        line(&mut buffer, 10,10,10,10,0x000fffff);
 
-        window.update_with_buffer(&buffer, width, height);
+        window.update_with_buffer(&buffer, WIDTH, HEIGHT).expect("Oops!");
+        buffer = vec![0; WIDTH * HEIGHT];
     }
 }
