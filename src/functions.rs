57ea3136426a1c2f76f657b01498faec9b109448
Cellinnek <Cysielek@outlook.com>
2022-11-01 11:30:24 +0100

    drawing faces

--- src/functions.rs
+++ src/functions.rs
@@ -1,5 +1,6 @@
 use crate::WIDTH;
 use crate::HEIGHT;
+use std::mem::swap;
 
 pub fn line(buffer: &mut Vec<u32>, argx1: i32, argy1: i32, argx2: i32, argy2: i32, color: u32) {
     let mut x = argx1;
@@ -32,7 +33,7 @@ pub fn line(buffer: &mut Vec<u32>, argx1: i32, argy1: i32, argx2: i32, argy2: i3
             break;
         };
 
-        err_tolerance = 1 * err;
+        err_tolerance = 2 * err;
 
         if err_tolerance > -dx {
             err -= dy;
@@ -46,6 +47,51 @@ pub fn line(buffer: &mut Vec<u32>, argx1: i32, argy1: i32, argx2: i32, argy2: i3
 }
 
 pub fn clear(buffer: &mut Vec<u32>,color:u32) {for i in 0..buffer.len(){buffer[i]=color;}}
+
+
+pub fn triangle(buffer: &mut Vec<u32>,mut x1:i32,mut y1:i32,mut x2:i32,mut y2:i32,mut x3:i32,mut y3:i32,color:u32){
+    let height = HEIGHT as i32;
+    let width = WIDTH as i32;
+    if y2 > y3
+    {
+        swap(&mut x2,&mut x3);
+        swap(&mut y2,&mut y3);
+    }
+    if y1 > y2
+    {
+        swap(&mut x1,&mut x2);
+        swap(&mut y1,&mut y2);
+    }
+    if y2 > y3
+    {
+
+        swap(&mut x2,&mut x3);
+        swap(&mut y2,&mut y3);
+    }
+    let dx_far = (x3 - x1) as f32/ (y3 - y1 + 1) as f32;
+    let dx_upper = (x2 - x1) as f32 / (y2 - y1 + 1) as f32;
+    let dx_low = (x3 - x2) as f32 / (y3 - y2 + 1) as f32;
+    let mut xf = x1 as f32;
+    let mut xt = x1 as f32 + dx_upper; // if y0 == y1, special case
+
+        for y in y1..(if y3<height-1{y3} else{height-1}) {
+            if y >= 0 {
+                for x in (if xf>0.0{xf as i32} else{0})..(if xt < (width-1) as f32{xt as i32} else{width-1}){
+                    buffer[(x+y*width) as usize] = color;
+                }
+
+                for x in (if xt > 0.0{xt as i32} else{0})..(if xf<width as f32{xf as i32} else{width-1}){
+                    buffer[(x+y*width) as usize] = color;
+                }
+
+            }
+            xf += dx_far;
+            if y < y2{xt += dx_upper;}
+            else{ xt += dx_low;}
+        }
+}
+
+
 
 #[derive(Copy, Clone)]
 pub struct Vertex {
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use minifb::{Scale, Window, WindowOptions};
 use std::time::Instant;
+use minifb::Key::Escape;
+use minifb::ScaleMode::AspectRatioStretch;
 
 const WIDTH: usize = 512;
 const HEIGHT: usize = 512;
 
 mod functions;
-use functions::{line,Vertex,rotate,clear};
+use functions::{line,Vertex,rotate,clear,triangle};
 
 fn main() {
     let mut cam_pos:(f32,f32,f32) = (0.0,0.0,1024.0);
-    let mut fov:f32 = 512.0;
+    let mut fov:f32 = WIDTH as f32;
 
     let mut ver: [Vertex; 8] = [
         Vertex{x:256.0,y:256.0,z:-256.0},//   0
@@ -22,7 +24,7 @@ fn main() {
         Vertex{x:-256.0,y:-256.0,z:256.0}//   7
     ];
 
-    let cube_edges: [[u16;2]; 12] = [
+    let cube_edges: [[u8;2]; 12] = [
         [0,1],
         [1,3],
         [3,2],
@@ -37,6 +39,14 @@ fn main() {
         [3,7]
     ];
 
+    let cube_faces: [[u8;3]; 4] = [
+        [0,1,3],
+        [0,2,3],
+        [4,5,7],
+        [4,6,7]
+    ];
+
+
     let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
 
     let mut window = Window::new(
@@ -44,17 +54,25 @@ fn main() {
         WIDTH,
         HEIGHT,
         WindowOptions {
+            resize: true,
+            scale_mode: AspectRatioStretch,
             ..WindowOptions::default()
         },
     ).unwrap();
 
-    /*window.limit_update_rate(Some(std::time::Duration::from_micros(16600/2)));*/
+    /*window.set_position(30,30);*/
+    /*window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));*/
 
     let mut now = Instant::now();
     let now2 = Instant::now();
-    while window.is_open() {
+    while window.is_open() && !window.is_key_down(Escape) {
+        let elapsed_time = now.elapsed().as_secs_f32();
+        now = Instant::now();
+        if now.duration_since(now2).as_millis()%30==0{
+            window.set_title(&((1.0/elapsed_time) as i32).to_string());
+        }
+        rotate(&mut ver,1.0*elapsed_time);
 
-        rotate(&mut ver,0.5*3.14/180.0);
         let cast_vet: [(i32,i32); 8] = [
             ver[0].cast(cam_pos,fov),
             ver[1].cast(cam_pos,fov),
@@ -72,16 +90,20 @@ fn main() {
             let y2 = cast_vet[i[1] as usize].1+HEIGHT as i32/2;
             line(&mut buffer,x1,y1, x2,y2, 0xffffffff);
         }
+        for i in cube_faces{
+            let x1 = cast_vet[i[0] as usize].0+WIDTH as i32/2;
+            let y1 = cast_vet[i[0] as usize].1+HEIGHT as i32/2;
+            let x2 = cast_vet[i[1] as usize].0+WIDTH as i32/2;
+            let y2 = cast_vet[i[1] as usize].1+HEIGHT as i32/2;
+            let x3 = cast_vet[i[2] as usize].0+WIDTH as i32/2;
+            let y3 = cast_vet[i[2] as usize].1+HEIGHT as i32/2;
+            triangle(&mut buffer,x1,y1,x2,y2,x3,y3,0xffffffff);
+        }
+
 
         /*buffer[((200 /*y*/ as usize) * (WIDTH)) + 200 /*x*/ as usize] = 0x00ffffff;*/
 
         window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
         clear(&mut buffer, 0);
-        let elapsed_time = 1.0/now.elapsed().as_secs_f32();
-        now = Instant::now();
-        if now.duration_since(now2).as_millis()%30==0{
-            window.set_title(&(elapsed_time as i32 *2).to_string());
-        }
-
     }
 }
